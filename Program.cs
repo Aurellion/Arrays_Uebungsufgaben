5cdbe512d6cf3dd82420b69be70b2406d33d3656
Aurellion <johannes.bock@nastor.eu>
2023-11-20 09:26:37 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Aufgabe 1
+            // a) Erzeugen Sie ein Array mit sieben
+            // Einträgen als Zahlen zwischen 1-99.
+            // b) Geben Sie die Summe aller Einträge und
+            // die größte und kleinste Zahl aus.
+            // c) Geben Sie das Array in umgekehrter Reihen-
+            // folge aus.
+
+            // Aufgabe 2
+            // a) Erzeugen Sie ein 2d-Array mit vier Zeilen
+            // und vier Spalten. Die Einträge wie in Aufgabe 1.
+            // b) Geben Sie das Array lesbar in der Konsole aus.
+            // c) Geben Sie nur die Diagonale des Array aus.
+            // Hinweis: int[,] Tabelle = new int[4,4];
+
+        }
+    }
+}
